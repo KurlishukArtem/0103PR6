007f99b724958aa71c9dc72907f01b55fb872a63
Hat <cool97438@gmail.com>
2024-11-22 16:33:35 +0500

    Разработанный код для переключения страниц. Kurlishuk

--- app/src/main/java/com/example/a6pr/MainActivity.java
+++ app/src/main/java/com/example/a6pr/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.a6pr;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.MotionEvent;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public int start_x=0;
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                start_x=(int)event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                if(Math.abs((int)event.getX()-start_x)>50){
+                    if(start_x<(int)event.getX()){
+                        setContentView(R.layout.signin);
+                    }
+                    else {
+                        setContentView(R.layout.regin);
+                    }
+                }
+
+        }
+        return false;
     }
 }
